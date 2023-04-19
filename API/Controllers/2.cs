aad7b5576fbee0357feb2ea00d60f79154a96b5f
shahin <shahin.kurdestan@gmail.com>
2023-04-19 12:17:35 +0330

    init 2

---
+++ API/Controllers/2.cs
@@ -0,0 +1,19 @@
+﻿using Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webchange_service.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class c_run_mail_to_kilometer_d_reverse_Controller : ControllerBase
+{
+    [HttpPost]
+    public double main(double c,double d)
+    {
+        Mailtokilometr mailtokilometr = new Mailtokilometr();
+        
+        return Mailtokilometr.B(c,d);
+       
+    }
+    
+}
---
+++ Service/Class2.cs
@@ -0,0 +1,19 @@
+﻿namespace Service;
+
+public class Mailtokilometr
+{
+    public static double B(double c, double d)
+    {
+        if (c >= 1)
+        {
+            double kilometer = c * 1.6;
+            return kilometer;
+        }
+        else
+        {
+            double mile = d / 1.6;
+            return mile;
+        }
+        
+    }
+}
